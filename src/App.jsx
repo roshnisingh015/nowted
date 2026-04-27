684b7c7b7c4c9b65cdf42faf08406c5d7cc4bc4a
retro <roshnisingh87090@gmail.com>
2026-04-27 16:23:42 +0530

    added base 3 column layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 function App() {
   return (
-    <div className="bg-black h-screen flex items-center justify-center">
-      <h1 className="text-white text-3xl font-bold">Hello Tailwind</h1>
+    <div className="flex h-screen w-screen bg-[#181818]">
+      
+     
+      <aside className="w-[300px] min-w-[300px] bg-[#181818]">
+        <p className="text-white p-4">Sidebar</p>
+      </aside>
+
+      
+      <div className="w-[350px] min-w-[350px] bg-[#252525]">
+        <p className="text-white p-4">Note List</p>
+      </div>
+
+      
+      <main className="flex-1 bg-[#1C1C1C]">
+        <p className="text-white p-4">Editor</p>
+      </main>
+
     </div>
   )
 }

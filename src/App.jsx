63946c6b3bc798a512cf379669ebf2cbf03ef1b6
retro <roshnisingh87090@gmail.com>
2026-04-27 13:54:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black h-screen flex items-center justify-center">
+      <h1 className="text-white text-3xl font-bold">Hello Tailwind</h1>
+    </div>
+  )
+}
+
+export default App
